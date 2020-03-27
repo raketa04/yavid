3bf047103b99bf15ef491f378f3bd3b882d5707d
3Cad-2 <raketa040794@gmail.com>
2020-03-27 16:06:45 +0300

    тест

--- src/main/java/by/yavid/Entity/Yavid/Variant.java
+++ src/main/java/by/yavid/Entity/Yavid/Variant.java
@@ -15,5 +15,19 @@ public class Variant {
     @Column(name="NAME_MAT")
     String memo;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
 }
